38a3e69226bd60d3c708eab785a5a96afccfffbd
Nitin Keswani <nitinjameskeswani@gmail.com>
2024-04-16 11:14:28 +1200

    Experimenting With Integers

--- chapter_two/using_integers.py
+++ chapter_two/using_integers.py
@@ -3,3 +3,7 @@ print(10-2)
 print(2*4)
 print(32/4)
 print(8**2)
+yearOfBirth = input("In which year were you born? ")
+intYearOfBirth = int(yearOfBirth)
+age = 2024 - intYearOfBirth
+print(f"If you were born in {yearOfBirth} then you must be approximately {age} years old!")
