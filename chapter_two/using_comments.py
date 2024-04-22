5a1bfedf5857ce750997db8b854be04a8d629a0e
Nitin Keswani <nitinjameskeswani@gmail.com>
2024-04-22 17:04:18 +1200

    Made a program with comments

---
+++ chapter_two/using_comments.py
@@ -0,0 +1,10 @@
+# This program uses comments!
+
+# Saying hello
+print("Hello there!")
+
+# Asking your age
+age = input("How old are you? ")
+
+# Telling you your age
+print(f"You are {age} years old!")
