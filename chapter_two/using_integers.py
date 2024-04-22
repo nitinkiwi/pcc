6b1e107e74be1e16a5e22d67260f86a959203840
Nitin Keswani <nitinjameskeswani@gmail.com>
2024-04-22 13:09:30 +1200

    Added the datetime module

--- chapter_two/using_integers.py
+++ chapter_two/using_integers.py
@@ -1,3 +1,6 @@
+import datetime
+today = datetime.date.today()
+year = today.year
 print(4+4)
 print(10-2)
 print(2*4)
@@ -5,5 +8,5 @@ print(32/4)
 print(8**2)
 yearOfBirth = input("In which year were you born? ")
 intYearOfBirth = int(yearOfBirth)
-age = 2024 - intYearOfBirth
+age = year - intYearOfBirth
 print(f"If you were born in {yearOfBirth} then you must be approximately {age} years old!")
