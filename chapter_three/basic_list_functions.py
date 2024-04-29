c06645f89ab66d031e122c3152e06700d8f2e193
Nitin Keswani <nitinjameskeswani@gmail.com>
2024-04-29 17:43:30 +1200

    Learned how to print list elements in strings

---
+++ chapter_three/basic_list_functions.py
@@ -0,0 +1,16 @@
+friends = ["George", "Dan", "Sean", "Sam"]
+print(friends[0])
+print(friends[1])
+print(friends[2])
+print(friends[3])
+
+print(f"One of my friends is {friends[0]}.")
+print(f"One of my friends is {friends[1]}.")
+print(f"One of my friends is {friends[2]}.")
+print(f"One of my friends is {friends[3]}.")
+
+f1_racers = ["leclerc", "verstappen", "hamilton", "alonso", "norris"]
+print(f"My favourite F1 racer is {f1_racers[0]}.")
+print(f"I find {f1_racers[1]} annoying.")
+print(f"I like the vibe of {f1_racers[2]}.")
+print(f"I also like the drivers {f1_racers[3]} and {f1_racers[4]}.")
