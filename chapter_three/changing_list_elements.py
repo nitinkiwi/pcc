890d6226121aa2a90923c5c3b7bada986b0cf66c
Nitin Keswani <nitinjameskeswani@gmail.com>
2024-04-29 17:41:12 +1200

    Learned how to add and remove list elements

--- chapter_three/changing_list_elements.py
+++ chapter_three/changing_list_elements.py
@@ -1,4 +1,15 @@
+# Appending list elements
 friends = ["George", "Dan", "Sean", "Sam"]
 print(friends)
 friends.append("Daniel")
 print(friends)
+# Inserting a value in a list
+friends.insert(0, "Tim")
+print(friends)
+friends.insert(2, "Emma")
+print(friends)
+# Deleting a value from a list
+del friends[0]
+print(friends)
+# Adding Tim in again (he is still my friend)
+friends.insert(0, "Tim")
